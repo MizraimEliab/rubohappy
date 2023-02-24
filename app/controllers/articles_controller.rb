28b3c9582c54eb400e2606848814eed7d5a92c9e
Mizraim Eliab <mizraim.avila@tiendasvertigo.mx>
2023-02-24 14:31:08 -0600

    exclude more files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,5 +13,6 @@ class ArticlesController < ApplicationController
     only = "SOlo estoy modificando un archivo"
     str = "STRING MALLLLL"
     money = "DINERO CON STRING MALLLLL"
+    mney = "String mal"
   end
 end
